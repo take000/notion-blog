cfd05349c3ddfc64cd1cd9610aa93c929b4338de
take000 <40623481+take000@users.noreply.github.com>
2022-10-21 03:56:38 +0900

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,67 @@
+import Head from "next/head";
+import Link from "next/link";
+import { getDatabase } from "../lib/notion";
+import { Text } from "./[id].js";
+import styles from "./index.module.css";
+
+export const databaseId = process.env.NOTION_DATABASE_ID;
+
+export default function Home({ posts }) {
+  return (
+    <div>
+      <Head>
+        <title>take000's blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.container}>
+        <header className={styles.header}>
+          <h1>take000's blog</h1>
+          <p>
+            HugoからNotion API + Next.jsに移行しました。
+          </p>
+        </header>
+
+        <h2 className={styles.heading}>All Posts</h2>
+        <ol className={styles.posts}>
+          {posts.map((post) => {
+            const date = new Date(post.last_edited_time).toLocaleString(
+              "en-US",
+              {
+                month: "short",
+                day: "2-digit",
+                year: "numeric",
+              }
+            );
+            return (
+              <li key={post.id} className={styles.post}>
+                <h3 className={styles.postTitle}>
+                  <Link href={`/${post.id}`}>
+                    <a>
+                      <Text text={post.properties.Name.title} />
+                    </a>
+                  </Link>
+                </h3>
+
+                <p className={styles.postDescription}>{date}</p>
+                <Link href={`/${post.id}`}>
+                  <a> Read post →</a>
+                </Link>
+              </li>
+            );
+          })}
+        </ol>
+      </main>
+    </div>
+  );
+}
+
+export const getStaticProps = async () => {
+  const database = await getDatabase(databaseId);
+  return {
+    props: {
+      posts: database,
+    },
+    revalidate: 1,
+  };
+};
